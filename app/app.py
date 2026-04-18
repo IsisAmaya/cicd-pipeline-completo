a7d5b4ca7244f0fae2aabb320f76c527d479c6bc
Isis Amaya <icamayaa@eafit.edu.co>
2026-04-18 12:23:48 -0500

    copy files of old repo

---
+++ app/app.py
@@ -0,0 +1,43 @@
+# app/app.py
+"""
+App web de una calculadora con flask
+"""
+
+from flask import Flask, render_template, request
+from .calculadora import sumar, restar, multiplicar, dividir
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    """
+    Manejar solicitudes get and post de la app calculadora
+    """
+    resultado = None
+    if request.method == "POST":
+        try:
+            num1 = float(request.form["num1"])
+            num2 = float(request.form["num2"])
+            operacion = request.form["operacion"]
+
+            if operacion == "sumar":
+                resultado = sumar(num1, num2)
+            elif operacion == "restar":
+                resultado = restar(num1, num2)
+            elif operacion == "multiplicar":
+                resultado = multiplicar(num1, num2)
+            elif operacion == "dividir":
+                resultado = dividir(num1, num2)
+            else:
+                resultado = "Operación no válida"
+        except ValueError:
+            resultado = "Error: Introduce números válidos"
+        except ZeroDivisionError:
+            resultado = "Error: No se puede dividir por cero"
+
+    return render_template("index.html", resultado=resultado)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    app.run(debug=False, port=5000, host="127.0.0.1")
